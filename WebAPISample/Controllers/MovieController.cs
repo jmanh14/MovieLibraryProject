518524c474dc2d35cfb6df35e69c1b029835116d
Justin Haldeman <jmanh14@gmail.com>
2020-03-30 15:30:28 -0500

    Added delete methd

--- WebAPISample/Controllers/MovieController.cs
+++ WebAPISample/Controllers/MovieController.cs
@@ -63,7 +63,10 @@ namespace WebAPISample.Controllers
         public IActionResult Delete(int id)
         {
             // Delete movie from db logic
-            return Ok();
+            var movie = _context.Movies.Where(a => a.MovieId == id).FirstOrDefault();
+            _context.Movies.Remove(movie);
+            _context.SaveChanges();
+            return Ok(_context.Movies.ToList());
         }
     }
 }
