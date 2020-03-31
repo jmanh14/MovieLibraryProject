9d97c11365e515e4eb02c3a1af3b6c1c04066982
Justin Haldeman <jmanh14@gmail.com>
2020-03-31 17:03:24 -0500

    90 percent done with edit method

--- Client/app.js
+++ Client/app.js
@@ -13,9 +13,9 @@
             contentType: 'application/json',
             data: JSON.stringify(dict),
             success: function( data, textStatus, jQxhr ){
-                $('#title td').append( function(data){
-                    data["title"];
-                } );
+                    $("#title").append(`<tr><td>${data["title"]}</td></tr>`);  
+                    $("#genre").append(`<tr><td>${data["genre"]}</td></tr>`);
+                    $("#director").append(`<tr><td>${data["director"]}\t<button onClick="Edit()">Edit</button>\t<button onClick="Delete()">Delete</button></td></tr>`);
             },
             error: function( jqXhr, textStatus, errorThrown ){
                 console.log( errorThrown );
@@ -29,12 +29,44 @@
 })(jQuery);
 
 $(function(){
+    LoadMovies();
+})
+
+function LoadMovies(){
     let data = {}
     $.get('https://localhost:44325/api/movie', function(data){
         for(let i = 0; i < data.length; i++){    
-            $("#title").append(`<tr><td>${data[i]["title"]}</td></tr>`);  
-            $("#genre").append(`<tr><td>${data[i]["genre"]}</td></tr>`);
-            $("#director").append(`<tr><td>${data[i]["director"]}</td></tr>`);
+            $("#movieTable").append(`<tr><td>${data[i]["title"]}</td><td>${data[i]["genre"]}</td><td>${data[i]["director"]}</td><td><button  onClick="Edit('${data[i]["movieId"]}', '${data[i]["title"]}', '${data[i]["genre"]}', '${data[i]["director"]}')">Edit</button></td></tr>`);  
         }
     })
-})
+}
+
+    function Edit(id, title, genre, director){
+        console.log("Edit");
+        id = parseInt(id);
+        var dict = {
+            MovieId: id,
+            Title: title,
+            Genre: genre,
+            Director: director
+        };
+        dict.Title = prompt("Enter in the new title:");
+        dict.Genre= prompt("Enter in the new genre:");
+        dict.Director = prompt("Enter in the new director:");
+            $.ajax({
+                url: 'https://localhost:44325/api/movie',
+                dataType: 'text',
+                type: 'PUT',
+                contentType: 'application/json',
+                data: JSON.stringify(dict),
+                success: function(data, textStatus, jQxhr){  
+                    document.body.innerHTML = "";
+                    LoadMovies();
+                    
+                },  
+                error: function( jqXhr, textStatus, errorThrown ){
+                    console.log( errorThrown );
+                }
+            });
+            
+    }
