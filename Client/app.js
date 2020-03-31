a8f745297405bdc80c0598f2a13bbc7c6b383ca1
Justin Haldeman <jmanh14@gmail.com>
2020-03-31 12:37:16 -0500

    Able to see all movies in html file

--- Client/app.js
+++ Client/app.js
@@ -13,7 +13,9 @@
             contentType: 'application/json',
             data: JSON.stringify(dict),
             success: function( data, textStatus, jQxhr ){
-                $('#response pre').html( data );
+                $('#title td').append( function(data){
+                    data["title"];
+                } );
             },
             error: function( jqXhr, textStatus, errorThrown ){
                 console.log( errorThrown );
@@ -25,3 +27,14 @@
 
     $('#my-form').submit( processForm );
 })(jQuery);
+
+$(function(){
+    let data = {}
+    $.get('https://localhost:44325/api/movie', function(data){
+        for(let i = 0; i < data.length; i++){    
+            $("#title").append(`<tr><td>${data[i]["title"]}</td></tr>`);  
+            $("#genre").append(`<tr><td>${data[i]["genre"]}</td></tr>`);
+            $("#director").append(`<tr><td>${data[i]["director"]}</td></tr>`);
+        }
+    })
+})
