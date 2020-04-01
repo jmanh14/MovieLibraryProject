481996ef53d0fa303fa4dd47e6a784cf895c3957
unknown <christinesoto21@gmail.com>
2020-04-01 07:03:12 -0500

    Fixed syntax issue

--- Client/app.js
+++ Client/app.js
@@ -6,68 +6,73 @@
 			Genre: this['genre'].value
 		};
 
-        $.ajax({
-            url: 'https://localhost:44325/api/movie',
-            dataType: 'json',
-            type: 'post',
-            contentType: 'application/json',
-            data: JSON.stringify(dict),
-            success: function( data, textStatus, jQxhr ){
-                    $("#title").append(`<tr><td>${data["title"]}</td></tr>`);  
-                    $("#genre").append(`<tr><td>${data["genre"]}</td></tr>`);
-                    $("#director").append(`<tr><td>${data["director"]}\t<button onClick="Edit()">Edit</button>\t<button onClick="Delete()">Delete</button></td></tr>`);
-            },
-            error: function( jqXhr, textStatus, errorThrown ){
-                console.log( errorThrown );
-            }
-        });
+		$.ajax({
+			url: 'https://localhost:44325/api/movie',
+			dataType: 'json',
+			type: 'post',
+			contentType: 'application/json',
+			data: JSON.stringify(dict),
+			success: function(data, textStatus, jQxhr) {
+				$('#title').append(`<tr><td>${data['title']}</td></tr>`);
+				$('#genre').append(`<tr><td>${data['genre']}</td></tr>`);
+				$('#director').append(
+					`<tr><td>${data[
+						'director'
+					]}\t<button onClick="Edit()">Edit</button>\t<button onClick="Delete()">Delete</button></td></tr>`
+				);
+			},
+			error: function(jqXhr, textStatus, errorThrown) {
+				console.log(errorThrown);
+			}
+		});
+	}
 
 	$('#my-form').submit(processForm);
 })(jQuery);
 
-$(function(){
-    LoadMovies();
-})
+$(function() {
+	LoadMovies();
+});
 
-function LoadMovies(){
-    let data = {}
-    $.get('https://localhost:44325/api/movie', function(data){
-        for(let i = 0; i < data.length; i++){    
-            $("#movieTable").append(`<tr><td>${data[i]["title"]}</td><td>${data[i]["genre"]}</td><td>${data[i]["director"]}</td><td><button  onClick="Edit('${data[i]["movieId"]}', '${data[i]["title"]}', '${data[i]["genre"]}', '${data[i]["director"]}')">Edit</button></td></tr>`);  
-        }
-    })
+function LoadMovies() {
+	let data = {};
+	$.get('https://localhost:44325/api/movie', function(data) {
+		for (let i = 0; i < data.length; i++) {
+			$('#movieTable').append(
+				`<tr><td>${data[i]['title']}</td><td>${data[i]['genre']}</td><td>${data[i][
+					'director'
+				]}</td><td><button  onClick="Edit('${data[i]['movieId']}', '${data[i]['title']}', '${data[i][
+					'genre'
+				]}', '${data[i]['director']}')">Edit</button></td></tr>`
+			);
+		}
+	});
 }
 
-    function Edit(id, title, genre, director){
-        console.log("Edit");
-        id = parseInt(id);
-        var dict = {
-            MovieId: id,
-            Title: title,
-            Genre: genre,
-            Director: director
-        };
-        dict.Title = prompt("Enter in the new title:");
-        dict.Genre= prompt("Enter in the new genre:");
-        dict.Director = prompt("Enter in the new director:");
-            $.ajax({
-                url: 'https://localhost:44325/api/movie',
-                dataType: 'text',
-                type: 'PUT',
-                contentType: 'application/json',
-                data: JSON.stringify(dict),
-                success: function(data, textStatus, jQxhr){  
-                    document.body.innerHTML = "";
-                    LoadMovies();
-                    
-                },  
-                error: function( jqXhr, textStatus, errorThrown ){
-                    console.log( errorThrown );
-                }
-            });
-            
-<<<<<<< HEAD
-    }
-=======
-    }
->>>>>>> 03101d4c4da3b2230a8308b2f0db62a6dfcd4976
+function Edit(id, title, genre, director) {
+	console.log('Edit');
+	id = parseInt(id);
+	var dict = {
+		MovieId: id,
+		Title: title,
+		Genre: genre,
+		Director: director
+	};
+	dict.Title = prompt('Enter in the new title:');
+	dict.Genre = prompt('Enter in the new genre:');
+	dict.Director = prompt('Enter in the new director:');
+	$.ajax({
+		url: 'https://localhost:44325/api/movie',
+		dataType: 'text',
+		type: 'PUT',
+		contentType: 'application/json',
+		data: JSON.stringify(dict),
+		success: function(data, textStatus, jQxhr) {
+			document.body.innerHTML = '';
+			LoadMovies();
+		},
+		error: function(jqXhr, textStatus, errorThrown) {
+			console.log(errorThrown);
+		}
+	});
+}
