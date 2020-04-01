ccc4dcbc1b9edc255c75adf6918238640ca108b7
Justin Haldeman <jmanh14@gmail.com>
2020-04-01 08:48:46 -0500

    Edit method works

--- Client/app.js
+++ Client/app.js
@@ -6,7 +6,6 @@
 			Genre: this['genre'].value
 		};
 
-<<<<<<< HEAD
         $.ajax({
             url: 'https://localhost:44325/api/movie',
             dataType: 'json',
@@ -23,28 +22,6 @@
             }
         });
     }
-=======
-		$.ajax({
-			url: 'https://localhost:44325/api/movie',
-			dataType: 'json',
-			type: 'post',
-			contentType: 'application/json',
-			data: JSON.stringify(dict),
-			success: function(data, textStatus, jQxhr) {
-				$('#title').append(`<tr><td>${data['title']}</td></tr>`);
-				$('#genre').append(`<tr><td>${data['genre']}</td></tr>`);
-				$('#director').append(
-					`<tr><td>${data[
-						'director'
-					]}\t<button onClick="Edit()">Edit</button>\t<button onClick="Delete()">Delete</button></td></tr>`
-				);
-			},
-			error: function(jqXhr, textStatus, errorThrown) {
-				console.log(errorThrown);
-			}
-		});
-	}
->>>>>>> 481996ef53d0fa303fa4dd47e6a784cf895c3957
 
 	$('#my-form').submit(processForm);
 })(jQuery);
@@ -53,7 +30,6 @@ $(function() {
 	LoadMovies();
 });
 
-<<<<<<< HEAD
 function LoadMovies(){
     let data = {}
     $.get('https://localhost:44325/api/movie', function(data){
@@ -93,47 +69,3 @@ function LoadMovies(){
             });
             
     }
-=======
-function LoadMovies() {
-	let data = {};
-	$.get('https://localhost:44325/api/movie', function(data) {
-		for (let i = 0; i < data.length; i++) {
-			$('#movieTable').append(
-				`<tr><td>${data[i]['title']}</td><td>${data[i]['genre']}</td><td>${data[i][
-					'director'
-				]}</td><td><button  onClick="Edit('${data[i]['movieId']}', '${data[i]['title']}', '${data[i][
-					'genre'
-				]}', '${data[i]['director']}')">Edit</button></td></tr>`
-			);
-		}
-	});
-}
-
-function Edit(id, title, genre, director) {
-	console.log('Edit');
-	id = parseInt(id);
-	var dict = {
-		MovieId: id,
-		Title: title,
-		Genre: genre,
-		Director: director
-	};
-	dict.Title = prompt('Enter in the new title:');
-	dict.Genre = prompt('Enter in the new genre:');
-	dict.Director = prompt('Enter in the new director:');
-	$.ajax({
-		url: 'https://localhost:44325/api/movie',
-		dataType: 'text',
-		type: 'PUT',
-		contentType: 'application/json',
-		data: JSON.stringify(dict),
-		success: function(data, textStatus, jQxhr) {
-			document.body.innerHTML = '';
-			LoadMovies();
-		},
-		error: function(jqXhr, textStatus, errorThrown) {
-			console.log(errorThrown);
-		}
-	});
-}
->>>>>>> 481996ef53d0fa303fa4dd47e6a784cf895c3957
