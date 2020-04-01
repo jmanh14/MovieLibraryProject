b7ddde66387b0d6d79b13272d16fb30b345717d9
Justin Haldeman <jmanh14@gmail.com>
2020-04-01 10:01:23 -0500

    Post function works again

--- Client/app.js
+++ Client/app.js
@@ -1,3 +1,7 @@
+$(function() {
+	LoadMovies();
+});
+
 (function($) {
 	function processForm(e) {
 		var dict = {
@@ -6,28 +10,6 @@
 			Genre: this['genre'].value
 		};
 
-<<<<<<< HEAD
-		$.ajax({
-			url: 'https://localhost:44325/api/movie',
-			dataType: 'json',
-			type: 'post',
-			contentType: 'application/json',
-			data: JSON.stringify(dict),
-			success: function(data, textStatus, jQxhr) {
-				$('#title').append(`<tr><td>${data['title']}</td></tr>`);
-				$('#genre').append(`<tr><td>${data['genre']}</td></tr>`);
-				$('#director').append(
-					`<tr><td>${data[
-						'director'
-					]}\t<button onClick="Edit()">Edit</button>\t<button onClick="deleteMovie()">Delete</button></td></tr>`
-				);
-			},
-			error: function(jqXhr, textStatus, errorThrown) {
-				console.log(errorThrown);
-			}
-		});
-	}
-=======
         $.ajax({
             url: 'https://localhost:44325/api/movie',
             dataType: 'json',
@@ -35,76 +17,30 @@
             contentType: 'application/json',
             data: JSON.stringify(dict),
             success: function( data, textStatus, jQxhr ){
-                $("#movieTable").append(`<tr><td>${data[i]["title"]}</td><td>${data[i]["genre"]}</td><td>${data[i]["director"]}</td><td><button  onClick="Edit('${data[i]["movieId"]}', '${data[i]["title"]}', '${data[i]["genre"]}', '${data[i]["director"]}')">Edit</button></td></tr>`);
+                $("#movieTable").append(`<tr><td>${data['title']}</td><td>${data["genre"]}</td><td>${data["director"]}</td><td><button onClick="Edit('${data["movieId"]}', '${data["title"]}', '${data["genre"]}', '${data["director"]}')">Edit</button><button onClick="DeleteMovie('${data["movieId"]}')">Delete</button></td></tr>`);  
             },
             error: function( jqXhr, textStatus, errorThrown ){
                 console.log( errorThrown );
             }
         });
+        e.preventDefault();
     }
->>>>>>> ccc4dcbc1b9edc255c75adf6918238640ca108b7
 
 	$('#my-form').submit(processForm);
 })(jQuery);
 
-$(function() {
-	LoadMovies();
-});
-
-<<<<<<< HEAD
-function LoadMovies() {
-	let data = {};
-	$.get('https://localhost:44325/api/movie', function(data) {
-		$('#movieTable').append(`<tr><th>Title</th><th>Genre</th><th>Director</th><th>Edit</th></tr>`);
-		for (let i = 0; i < data.length; i++) {
-			$('#movieTable').append(
-				`<tr><td>${data[i]['title']}</td><td>${data[i]['genre']}</td><td>${data[i][
-					'director'
-				]}</td><td><button  onClick="Edit('${data[i]['movieId']}', '${data[i]['title']}', '${data[i][
-					'genre'
-				]}', '${data[i]['director']}')">Edit</button></td></tr>`
-			);
-		}
-	});
-}
-
-function Edit(id, title, genre, director) {
-	console.log('Edit');
-	id = parseInt(id);
-	var dict = {
-		MovieId: id,
-		Title: title,
-		Genre: genre,
-		Director: director
-	};
-	dict.Title = prompt('Enter in the new title:');
-	dict.Genre = prompt('Enter in the new genre:');
-	dict.Director = prompt('Enter in the new director:');
-	$.ajax({
-		url: 'https://localhost:44325/api/movie',
-		dataType: 'text',
-		type: 'PUT',
-		contentType: 'application/json',
-		data: JSON.stringify(dict),
-		success: function(data, textStatus, jQxhr) {
-			document.getElementById('movieTable').innerHTML = '';
-			LoadMovies();
-		},
-		error: function(jqXhr, textStatus, errorThrown) {
-			console.log(errorThrown);
-		}
-	});
-}
 //Delete
-function deleteMovie(id) {
+function DeleteMovie(id) {
+    id = parseInt(id);
 	$.ajax({
-		url: 'https://localhost:44325/api/movie?',
+		url: 'https://localhost:44325/api/movie/'+id,
 		dataType: 'text',
 		type: 'delete',
 		contentType: 'application/json',
 		data: JSON.stringify(id),
 		success: function(data) {
-			alert('Movie deleted from Library');
+            alert('Movie deleted from Library');
+            document.getElementById("movieTable").innerHTML = "";
 			LoadMovies();
 		},
 		error: function(errorThrown) {
@@ -112,13 +48,12 @@ function deleteMovie(id) {
 		}
 	});
 }
-=======
 function LoadMovies(){
     let data = {}
     $.get('https://localhost:44325/api/movie', function(data){
-        $("#movieTable").append(`<tr><th>Title</th><th>Genre</th><th>Director</th><th>Edit</th></tr>`); 
+        $("#movieTable").append(`<tr><th>Title</th><th>Genre</th><th>Director</th></tr>`); 
         for(let i = 0; i < data.length; i++){     
-            $("#movieTable").append(`<tr><td>${data[i]["title"]}</td><td>${data[i]["genre"]}</td><td>${data[i]["director"]}</td><td><button  onClick="Edit('${data[i]["movieId"]}', '${data[i]["title"]}', '${data[i]["genre"]}', '${data[i]["director"]}')">Edit</button></td></tr>`);  
+            $("#movieTable").append(`<tr><td>${data[i]["title"]}</td><td>${data[i]["genre"]}</td><td>${data[i]["director"]}</td><td><button onClick="Edit('${data[i]["movieId"]}', '${data[i]["title"]}', '${data[i]["genre"]}', '${data[i]["director"]}')">Edit</button><button onClick="DeleteMovie('${data[i]["movieId"]}')">Delete</button></td></tr>`);  
         }
     })
 }
@@ -152,4 +87,3 @@ function LoadMovies(){
             });
             
     }
->>>>>>> ccc4dcbc1b9edc255c75adf6918238640ca108b7
